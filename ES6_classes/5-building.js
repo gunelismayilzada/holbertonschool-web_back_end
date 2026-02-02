2309ca6af997b7a18516038f5627770d03317cb5
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-02-02 20:07:39 +0000

    commit

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/prefer-default-export
+export default class Building {
+    constructor(sqft) {
+      this.sqft = sqft;
+      if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+  
+    set sqft(MYSQFT) {
+      this._sqft = MYSQFT;
+    }
+  }
