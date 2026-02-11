850f4c6607b32bfdedc3910cd0a7499e8eeda20e
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-02-11 16:53:50 +0000

    comit

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+    if (startString === '' || typeof startString !== 'string') {
+      return '';
+    }
+    const myArr = [];
+    for (const item of set) {
+      if (typeof item === 'string' && item.startsWith(startString)) {
+        myArr.push(item.slice(startString.length));
+      }
+    }
+    return myArr.join('-');
+  }
