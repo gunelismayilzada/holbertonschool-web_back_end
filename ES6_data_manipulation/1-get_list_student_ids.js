680983718dc99584bdab99c403a51731dde074f7
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-02-11 16:45:11 +0000

    comit

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(getListStudents) {
+    if (!(getListStudents instanceof Array)) {
+      return [];
+    }
+    const myArray = getListStudents.map((x) => x.id);
+    return myArray;
+  }
