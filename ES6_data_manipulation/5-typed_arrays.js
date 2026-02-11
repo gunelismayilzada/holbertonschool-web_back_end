c1bfcc996afa5700840b8c5ed614e8d7102c1ee9
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-02-11 16:50:46 +0000

    comit

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+export default function createInt8TypedArray(length, position, value) {
+    const myAB = new ArrayBuffer(length);
+    const myView = new DataView(myAB, 0);
+    const my8Array = new Int8Array(myAB);
+    for (let x = 0; x < length; x += 1) {
+      if (position > length) {
+        throw new Error('Position outside range');
+      } if (x === position) {
+        my8Array[x] = value;
+      }
+    }
+    return myView;
+  }
