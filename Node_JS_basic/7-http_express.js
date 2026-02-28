ee4e49c53755c7259d4172b802cee85641f4dfa5
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-02-28 08:19:04 +0000

    Node

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  await countStudents(process.argv[2])
+    .then((data) => {
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${data.CS.num + data.SWE.num}\n`);
+      res.write(`Number of students in CS: ${data.CS.num}. List: ${data.CS.list}\n`);
+      res.write(`Number of students in SWE: ${data.SWE.num}. List: ${data.SWE.list}`);
+      res.end();
+    })
+    .catch((err) => {
+      res.write('This is the list of our students\n');
+      res.end(err.message);
+    });
+}).listen(port);
+
+module.exports = app;
