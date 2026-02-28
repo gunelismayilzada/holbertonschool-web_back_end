f3d342c6fe66ab3d37b2cc8a6dda20e4f4094761
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-02-28 08:15:57 +0000

    Node

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+      await countStudents(process.argv[2])
+        .then((data) => {
+          res.write('This is the list of our students\n');
+          res.write(`Number of students: ${data.CS.num + data.SWE.num}\n`);
+          res.write(`Number of students in CS: ${data.CS.num}. List: ${data.CS.list}\n`);
+          res.write(`Number of students in SWE: ${data.SWE.num}. List: ${data.SWE.list}`);
+          res.end();
+        })
+        .catch((err) => {
+          res.write('This is the list of our students\n');
+          res.end(err.message);
+        });
+    }
+  }
+}).listen(1245);
+
+module.exports = app;
